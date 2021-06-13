64fed5367282334ecc4614c029235603d59fb7d3
paul468 <paul1@gyurma.de>
2021-06-13 14:09:46 +0200

    wha

--- game/colors/colors.py
+++
@@ -1,8 +0,0 @@
-def color(text):
-    string = ""
-    for l in text["before"]:
-        string += l
-    string += text["string"]
-    for l in text["after"]:
-        string += l
-    return string
